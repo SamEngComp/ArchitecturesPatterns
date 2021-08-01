e0979b4dfa41a5be11e3c7312228adf2b297ce27
SamEngComp <salessamuel268@gmail.com>
2021-08-01 01:41:17 -0300

    Added in Model - TrafficLight and TrafficLightService

---
+++ ExampleMVP/ExampleMVP/Model/TrafficLightService.swift
@@ -0,0 +1,24 @@
+//
+//  TrafficLightService.swift
+//  ExampleMVP
+//
+//  Created by Samuel Sales on 01/08/21.
+//
+
+import Foundation
+
+class TrafficLightService {
+    
+    func getTrafficLight(colorName:(String), callBack:(TrafficLight?) -> Void) {
+        let trafficLights = [TrafficLight(colorName: "Red", description: "Stop"),
+                             TrafficLight(colorName: "Green", description: "Go"),
+                             TrafficLight(colorName: "Yellow", description: "About to change to red")
+        ]
+        
+        if let foundTrafficLight = trafficLights.first(where: {$0.colorName == colorName}) {
+            callBack(foundTrafficLight)
+        } else {
+            callBack(nil)
+        }
+    }
+}
